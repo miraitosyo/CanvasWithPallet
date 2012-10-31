e88c8c86f12eddaf89efc629e9ea71a55dc27271
古川 涼太 <ryota@furukawa-no-MacBook-Pro.local>
2012-10-31 16:35:23 +0900

    Initial Commit

---
+++ CanvasWithPallet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CanvasWithPallet
+//
+//  Created by 古川 涼太 on 12/10/31.
+//  Copyright (c) 2012年 古川 涼太. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
